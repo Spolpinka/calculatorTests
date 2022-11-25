0dab6e29e773405dbdc280c4bb1b40c970f53964
Spolpinka <a421243266@gmail.com>
2022-11-25 10:33:50 +0300

    Initial commit

---
+++ src/main/java/pro/sky/calculator/calculatortests/CalculatorTestsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator.calculatortests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/calculator/calculatortests/CalculatorTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculator.calculatortests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
